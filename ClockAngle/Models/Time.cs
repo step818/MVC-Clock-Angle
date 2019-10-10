01fa1eb486bc0395b02725f534bcd7324be38f94
Stephen Trewick <stephent818@gmail.com>
2019-10-09 17:22:35 -0700

    valid hour input works

--- ClockAngle/Models/Time.cs
+++ ClockAngle/Models/Time.cs
@@ -14,6 +14,17 @@ namespace ClockAngle.Models
       Minute = minute;
       _angle = 0;
     }
+    public string Validate()
+    {
+      if(Hour < 1 || Hour > 12)
+      {
+        return "Error: Please enter a valid time";
+      }
+      else
+      {
+        return "";
+      }
+    }
     public int Angle()
     {
       if(Hour == 12)
@@ -22,15 +33,29 @@ namespace ClockAngle.Models
       }
       int degreesInHrs = Hour * 30;
       int degreesInMins = Minute * 6;
+      int constantMovingHour = (Minute/10);
+      Console.WriteLine(Minute/10);
       if ((Minute/5) >= Hour)
       {
-        _angle = degreesInMins - degreesInHrs;
+        _angle = (degreesInMins-constantMovingHour) - degreesInHrs;
+      if(_angle < 0)
+        {
+          _angle = _angle * -1;
+        }
       }
       else
       {
         _angle = degreesInHrs - degreesInMins;
+        if(_angle < 0)
+        {
+          _angle = _angle * -1;
+        }
       }
       return _angle;
+    }
+    public int ExplementaryAngle()
+    {
+      return (360 - _angle);
     }
   }
 }
